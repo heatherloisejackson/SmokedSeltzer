2fa90615de69a7d7dfb1737594a37c8ceff27f6e
Heather Jackson <hmjackson917@gmail.com>
2022-02-20 01:15:24 -0500

    fix email route

--- script.js
+++ script.js
@@ -6,16 +6,14 @@ contactLink.addEventListener("click", function () {
   var newName = document.getElementById("newName").value,
     newEmail = document.getElementById("newEmail").value,
     /* the subject variable holds the subject of the email, change its value per your requirement. */
-    subject = "New Email Submitted to SmokedSeltzer Waitlist",
+    subject = "I want to joing the SmokedSeltzer Waitlist!",
     /* the queryString variable holds the email's subject and body to be used in the href attribute of the 'a' tag. the '\n' character is used to make a new line and it must be encoded, along with other special characters as the space, in a valid URL format, we'll be using the built-in 'encodeURI' method for that task. */
     queryString =
       "?subject=" + subject + "&body=Name: " + newName + "\nEmail: " + newEmail,
     /* the emailAddr variable holds the email which you want the email to be sent to. Change its value per your requirement. */
-    emailAddr = "hmjackson917@gmail.com";
+    emailAddr = "waitlist@smokedseltzer.com";
   /* assign the href attribute of the 'a' tag by the queryString variable's value prepended by the desired email adress, and encode all the resulted string in the URL format using the built-in 'encodeURI' method. */
   this.href = window.encodeURI("mailto:" + emailAddr + queryString);
   // just for testing to see the resulted href string. Delete this line in production phase.
-  console.log(this.href);
+  // console.log(this.href);
 });
-
-console.log("hello world");
